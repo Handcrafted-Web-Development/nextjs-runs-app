d49488eb4e695e49fa9b9d6f19e9767e4cb65b21
Rose DORLÉANS <rose.dorleans@autobonplan.com>
2024-02-21 13:55:24 +0100

    feat: add components to index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+
+// import TargetChoice from '../components/pages/targetPage'
+import CharacterChoice from '../components/pages/characterPage'
+
 export default function Home() {
     return (
-        <>
-        </>
+
+        // <TargetChoice />
+
+        <CharacterChoice />
     );
 }
