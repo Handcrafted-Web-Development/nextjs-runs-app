c3ccbb1fcdc882315b77c75ff424975a6b7ca1d4
Lucas-Debiais <lucas.debiais11@gmail.com>
2024-02-22 16:26:09 +0100

    fix: skills

--- src/components/Skills/Skill.tsx
+++ src/components/Skills/Skill.tsx
@@ -1,23 +1,21 @@
-import { ReactElement } from 'react';
-import { SkillProps } from '@/services/interfaces/Skill';
+import {ReactElement} from 'react';
+import {SkillProps} from '@/services/interfaces/Skill';
 
 import '@/styles/skills.scss';
 
-const Skill = ({ name, value }: SkillProps): ReactElement => {
-  console.log(name);
-  console.log(value);
+const Skill = ({name, value}: SkillProps): ReactElement => {
 
-  return (
-    <div className={`skill ${name}`}>
-      <div className="skill_content">
-        <svg className="icon">
-          <use href={`/assets/img/icons.svg#${name}`} />
-        </svg>
-        <span>{name}</span>
-      </div>
-      <span style={{ height: `${value}px` }} className={value == 100 ? `progress progress-100` : `progress`}></span>
-    </div>
-  );
+    return (
+        <div
+            className={`skill ${name}`}
+            style={{backgroundImage: `linear-gradient(to top, rgba(0,0,0,.1) 0%, rgba(0,0,0,.1) ${value}%, transparent ${value}%, transparent 100%`}}
+        >
+            <svg className="icon">
+                <use href={`/assets/img/icons.svg#${name}`}/>
+            </svg>
+            <span>{name}</span>
+        </div>
+    );
 };
 
 export default Skill;
