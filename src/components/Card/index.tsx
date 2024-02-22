4c87896c0f1393bea4578f6dc649445be6fe3803
Lucas-Debiais <lucas.debiais11@gmail.com>
2024-02-22 12:58:36 +0100

    feat: card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,16 +1,17 @@
+import "@/styles/card.scss";
 import { ReactElement } from 'react';
 import Button from '@/components/Buttons/Button';
 import { CardProps } from '@/services/interfaces/Card';
 const Index = ({ label, name, description, choices }: CardProps): ReactElement => {
   return (
-    <div>
-      <span>{label}</span>
-      <div>
-        <div>
+    <div className="card">
+      <span className="card__label">{label}</span>
+      <div className="card__contain">
+        <div className="card__text">
           <h3>{name}</h3>
           <p>{description}</p>
         </div>
-        <div>
+        <div className="card__buttons">
           {choices?.map((choice, index) => (
             <Button key={index} value={choice.value} choicesLenght={choices.length} choicePlace={choice.place} />
           ))}
