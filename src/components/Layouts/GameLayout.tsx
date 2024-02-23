193ff5fd27bdfe16e6974403a61752cb44c4bf60
Lucas-Debiais <lucas.debiais11@gmail.com>
2024-02-23 15:12:30 +0100

    feat: animate

--- src/components/Layouts/GameLayout.tsx
+++ src/components/Layouts/GameLayout.tsx
@@ -1,73 +1,79 @@
-import { ReactElement, useEffect, useState } from 'react';
+import {ReactElement, useEffect, useState} from 'react';
 import Skills from '@/components/Skills';
 import Timeline from '@/components/Timeline';
 import CardComponent from '@/components/Card';
-import { Card } from '@/classes/Card';
-import { ChoiceProps } from '@/services/interfaces/Card';
-import { GameInstance } from '@/classes/GameInstance';
+import {Card} from '@/classes/Card';
+import {ChoiceProps} from '@/services/interfaces/Card';
+import {GameInstance} from '@/classes/GameInstance';
 
 const GameLayout = ({
-  choicesJson,
-  gameInstance,
-}: {
-  choicesJson: Card[];
-  gameInstance: GameInstance;
+                        choicesJson,
+                        gameInstance,
+                    }: {
+    choicesJson: Card[];
+    gameInstance: GameInstance;
 }): ReactElement => {
-  const [cards, setCards] = useState<Card[]>();
-  const setCard = (item: Card) => {
-    const newCard = new Card(item.label, item.name, item.description, item.choices, item.img);
-    setCards((prevCards) => [...(prevCards || []), newCard]);
-  };
+    const [cards, setCards] = useState<Card[]>();
+    const setCard = (item: Card) => {
+        const newCard = new Card(item.label, item.name, item.description, item.choices, item.img);
+        setCards((prevCards) => [...(prevCards || []), newCard]);
+    };
 
-  useEffect(() => {
-    setCard(choicesJson[Math.floor(Math.random() * choicesJson.length)]);
-    setCard(choicesJson[Math.floor(Math.random() * choicesJson.length)]);
-  }, [choicesJson]);
+    useEffect(() => {
+        setCard(choicesJson[Math.floor(Math.random() * choicesJson.length)]);
+        setCard(choicesJson[Math.floor(Math.random() * choicesJson.length)]);
+    }, [choicesJson]);
 
-  //Quand on click sur le bouton
-  const handleButtonClick = (choice: ChoiceProps) => {
-    //On met à jour les stats
-    gameInstance.updateStats(choice);
+    //Quand on click sur le bouton
+    const handleButtonClick = (choice: ChoiceProps) => {
+        //On met à jour les stats
+        gameInstance.updateStats(choice);
 
-    //Detection défaite
-    gameInstance.detectDefeat();
+        //Detection défaite
+        gameInstance.detectDefeat();
 
-    //Detection victoire
-    gameInstance.detectVictory();
+        //Detection victoire
+        gameInstance.detectVictory();
 
-    //Avance d'une étape dans la Timeline
-    gameInstance.updateTimeline();
+        //Avance d'une étape dans la Timeline
+        gameInstance.updateTimeline();
 
-    //On vient supprimer la carte sur laquelle on a cliqué
-    setCards((prevCards) => {
-      return prevCards?.slice(1) || [];
-    });
+        //On vient ajouter une classe d'aniamtion sur la card
+        choice.place === 'right' ? document.querySelectorAll('.card')[0].classList.add('swipe','swipe-right') :document.querySelectorAll('.card')[0].classList.add('swipe','swipe-left')
 
-    //On en crée une nouvelle derrière
-    setCard(choicesJson[Math.floor(Math.random() * choicesJson.length)]);
-  };
+            setTimeout(() => {
+                document.querySelectorAll('.card')[0].classList.remove('swipe','swipe-right','swipe-left')
+                //On vient supprimer la carte sur laquelle on a cliqué
+                setCards((prevCards) => {
+                    return prevCards?.slice(1) || [];
+                });
 
-  const cardComponents = cards?.map((card, index) => (
-    <CardComponent
-      key={index}
-      onClick={(choice) => handleButtonClick(choice)}
-      label={card.label}
-      name={card.name}
-      description={card.description}
-      img={card.img}
-      choices={card.choices}
-    />
-  ));
+                //On en crée une nouvelle derrière
+                setCard(choicesJson[Math.floor(Math.random() * choicesJson.length)])
+            }, 500)
+    };
 
-  let currentStep = gameInstance.getTimeline() ?? '';
+    const cardComponents = cards?.map((card, index) => (
+        <CardComponent
+            key={index}
+            onClick={(choice) => handleButtonClick(choice)}
+            label={card.label}
+            name={card.name}
+            description={card.description}
+            img={card.img}
+            choices={card.choices}
+        />
+    ));
 
-  return (
-    <>
-      <Skills />
-      <Timeline currentStep={currentStep} steps={15} />
-      <div className="cards">{cardComponents}</div>
-    </>
-  );
+    let currentStep = gameInstance.getTimeline() ?? '';
+
+    return (
+        <>
+            <Skills/>
+            <Timeline currentStep={currentStep} steps={15}/>
+            <div className="cards">{cardComponents}</div>
+        </>
+    );
 };
 
 export default GameLayout;
