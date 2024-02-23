8faabf748d78d74e95fe4e5905e8dcab3302e089
Lucas-Debiais <lucas.debiais11@gmail.com>
2024-02-23 13:26:29 +0100

    remove: dead code

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -10,32 +10,4 @@ export class Player {
     this.fitness = fitness;
     this.money = money;
   }
-
-  // getStats(): [string, number][] {
-  //     return [
-  //         ['social', this.social],
-  //         ['motivation', this.motivation],
-  //         ['fitness', this.fitness]
-  //         ['money', this.money],
-  //     ];
-  // }
-
-  changeStat(stat: string, value: number): void {
-    switch (stat) {
-      case 'social':
-        this.social += value;
-        break;
-      case 'motivation':
-        this.motivation += value;
-        break;
-      case 'fitness':
-        this.fitness += value;
-        break;
-      case 'money':
-        this.money += value;
-        break;
-      default:
-        console.error('Not a stat');
-    }
-  }
 }
